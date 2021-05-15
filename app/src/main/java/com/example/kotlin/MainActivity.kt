a8d5e7170db960828bf33be4a76a2a3059e3e831
hemant <hemantkumar692@gmail.com>
2021-05-15 21:22:38 +0530

    option menu added

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -3,21 +3,49 @@ package com.example.kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(abc:Bundle?){
         super.onCreate(abc)
         setContentView(R.layout.activity_main)
-     val edttext = findViewById<EditText>(R.id.ed)
-        val button = findViewById<Button>(R.id.submit)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
 
-        button.setOnClickListener(){
-            val mystring = edttext.text.toString()
-            val mytoast = Toast.makeText(applicationContext,edttext.text,Toast.LENGTH_SHORT).show()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+      menuInflater.inflate(R.menu.menutwo,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.setting -> {
+                Toast.makeText(applicationContext,"setting clicked",Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.share -> {
+                Toast.makeText(applicationContext,"Share clicked",Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.exit -> {
+                Toast.makeText(applicationContext,"Exit clicked",Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.profile -> {
+                Toast.makeText(applicationContext,"Profile clicked",Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> {
+                Toast.makeText(applicationContext,"Nothing  clicked",Toast.LENGTH_SHORT).show()
+                true
+            }
         }
     }
 }
