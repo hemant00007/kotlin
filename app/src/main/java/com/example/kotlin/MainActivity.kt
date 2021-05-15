d85c97336d6ac931a88822e0704713e630c3b244
hemant <hemantkumar692@gmail.com>
2021-05-15 16:38:40 +0530

    toolbar, contextMenu

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,41 +2,59 @@ package com.example.kotlin
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(abc:Bundle?) {
         super.onCreate(abc)
         setContentView(R.layout.activity_main)
-
-        val btn= findViewById<Button>(R.id.alert)
-        btn.setOnClickListener(){
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("My Dialog")
-            builder.setMessage("This is message")
-            builder.setIcon(android.R.drawable.btn_dialog)
-            builder.setPositiveButton("Yes"){
-                dialog: DialogInterface?, which: Int ->
-                Toast.makeText(this,"cliced yes",Toast.LENGTH_SHORT).show()
-            }
-            builder.setNegativeButton("No"){
-                dialog: DialogInterface?, which: Int ->
-                Toast.makeText(this,"clicked No",Toast.LENGTH_SHORT).show()
-            }
-            val alertdialo : AlertDialog = builder.create()
-            alertdialo.setCancelable(true)
-            alertdialo.show()
-
-        }
+      val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setTitle("Hello Kotlin")
+        toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(toolbar)
 
 
+      val listview = findViewById<ListView>(R.id.listview)
+        val myarraylist = arrayOf<String>("Hemant","Lucky","Neetu","pooja","Prity","Beauty","arti","prakash","mainish","Mukul")
+        val arrayAdapter : ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,myarraylist)
 
+        listview.adapter=arrayAdapter
+        registerForContextMenu(listview)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater  = menuInflater
+        inflater.inflate(R.menu.main_menu,menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+       return when(item !!.itemId){
+           R.id.call ->{
+               Toast.makeText(this,"call ",Toast.LENGTH_SHORT).show()
+               return true
+           }
+           R.id.sms -> {
+               Toast.makeText(this,"sms ",Toast.LENGTH_SHORT).show()
+               return true
+           }
+           else -> super.onOptionsItemSelected(item)
+       }
     }
 }
 
