023f1e44133e085987caa340c55290353a596925
hemant <hemantkumar692@gmail.com>
2021-05-17 17:29:06 +0530

    Recyclerview example

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,53 +2,19 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity(){
+    private val movielist = ArrayList<Model>()
+
     override fun onCreate(abc:Bundle?){
         super.onCreate(abc)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+  //  val recyclerview : Recy
+      //  https://www.tutorialspoint.com/working-with-recyclerview-in-an-android-app-using-kotlin
+    }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-      menuInflater.inflate(R.menu.menutwo,menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
-            R.id.setting -> {
-                Toast.makeText(applicationContext,"setting clicked",Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.share -> {
-                Toast.makeText(applicationContext,"Share clicked",Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.exit -> {
-                Toast.makeText(applicationContext,"Exit clicked",Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.profile -> {
-                Toast.makeText(applicationContext,"Profile clicked",Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> {
-                Toast.makeText(applicationContext,"Nothing  clicked",Toast.LENGTH_SHORT).show()
-                true
-            }
-        }
-    }
-}
 
 
 
